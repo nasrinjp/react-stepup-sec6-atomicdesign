d730143cfe12f31c69811000371c78cc55d6b4a7
Takashi Nasu <nasrinjp@gmail.com>
2021-04-13 11:04:33 +0900

    29.Organisms作成1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-9999-8888</dd>
+        <dt>会社名</dt>
+        <dd>ああああああ</dd>
+        <dt>Web</dt>
+        <dd>aaa.com</dd>
+      </dl>
+    </div>
+  );
+};
