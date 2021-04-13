3c74b502ec65aa434b86eb18a4c3e36780b86b59
Takashi Nasu <nasrinjp@gmail.com>
2021-04-13 14:25:24 +0900

    37.state管理(ユーザ情報の設定と参照)

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -18,17 +17,13 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
-
-  console.log(state);
   return (
     <SContainer>
       <SUserArea>
         <h2>ユーザ一覧</h2>
         <SearchInput />
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={user.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,12 +1,14 @@
-import react, { createContext } from "react";
+import react, { createContext, useState } from "react";
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
+
+  const [userInfo, setUserInfo] = useState(null);
   const contextName = "テスト";
   return (
-    <UserContext.Provider value={{ contextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
